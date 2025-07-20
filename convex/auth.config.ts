ea0ed6cf64b2f8a550611d81fd8d63ab7ba92600
Rahat Moktadir <rahatmoktadir03@gmail.com>
2025-07-19 22:47:00 -0400

    Update auth config for production

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,9 @@
 export default {
   providers: [
     {
-      domain: "https://super-escargot-46.clerk.accounts.dev",
+      domain:
+        process.env.CLERK_ISSUER_URL ||
+        "https://super-escargot-46.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
